b0c64fb0298f8613e2a018e945a95e1ed9e01626
Iulia Calatoaie <iulia.calatoaie@gmail.com>
2021-06-03 10:41:41 +0300

    ex 1

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/utils/Constants.java
@@ -0,0 +1,11 @@
+package ro.pub.cs.systems.eim.practicaltest02.utils;
+
+public class Constants {
+    final public static boolean DEBUG = true;
+
+    final public static String TAG = "[Practical Test 02]";
+
+    final public static Integer PORT = 2000;
+    final public static Integer WEBSITE_PORT = 13;
+    final public static String WEBSITE_URL = "utcnist.colorado.edu";
+}
